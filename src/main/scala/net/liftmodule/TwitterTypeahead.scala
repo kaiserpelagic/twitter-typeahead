2f710ca2a7e4af6d5e4390b912b1847809a127b5
greg <greg.flanagan@locusenergy.com>
2013-08-01 14:41:19 -0700

    don't need to register this anymore to the dispatch

--- src/main/scala/net/liftmodule/TwitterTypeahead.scala
+++ src/main/scala/net/liftmodule/TwitterTypeahead.scala
@@ -1,12 +1,10 @@
 package net.liftmodules
 
 import _root_.net.liftweb.http._
-import net.liftmodules.typeahead.{TypeaheadRest}
 
 object TwitterTypeahead {
 
   def init() {
     LiftRules.addToPackages("net.liftmodules.typeahead")
-    LiftRules.statelessDispatch.append(TypeaheadRest)
   }
 }
